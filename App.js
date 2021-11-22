e2b8cf404406a8864dd9ec7d67df09b16bffa126
DAYOON0836 <dayoon0836@ewhain.net>
2021-11-22 15:56:27 +0900

    RANDOM SCREEN 구현

--- App.js
+++ App.js
@@ -1,21 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'; 
+import { NavigationContainer } from '@react-navigation/native'; 
+import { createStackNavigator } from '@react-navigation/stack'; 
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import RandomScreen from './src/screens/Random'; 
+import RandomD from './src/screens/Random2'; 
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createStackNavigator(); 
+function App() { 
+    return ( 
+    <NavigationContainer> 
+        <Stack.Navigator initialRouteName="Random"> 
+            <Stack.Screen name="Random" component={RandomScreen} 
+                options={{ 
+                    title: 'Random',
+                    headerTintColor: '#00462a',
+                    headerTitleStyle: {
+                    fontSize: 36,
+                },
+                }}/> 
+            <Stack.Screen name="Random2" component={RandomD} 
+                options={{ 
+                    title: 'Random', 
+                    headerTintColor: '#00462a',
+                    headerTitleStyle: {
+                    fontSize: 36,
+                },
+                }}/> 
+         </Stack.Navigator> 
+     </NavigationContainer> 
+     );
+}; 
+     
+     
+export default App;
---
+++ src/styles.js
@@ -0,0 +1,120 @@
+import { StyleSheet } from "react-native";
+import {theme} from './theme';
+
+export const viewStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.background,
+    alignItems: 'center',
+  },
+  container2: {
+    flex: 1,
+    backgroundColor: theme.background,
+    alignItems: 'center',
+  },
+  item1: {
+    flexDirection: 'row',
+    top: 10,
+    width:390,
+    height: 'auto',
+    padding: 15,
+    backgroundColor: '#e6e6e6',
+    alignItems: 'center', 
+  },
+  item2: {
+    top: 10,
+    width:390,
+    height: 170,
+    padding: 15,
+    justifyContent: 'center',
+    backgroundColor: '#e6e6e6',
+  },
+});
+
+export const textStyles = StyleSheet.create({
+  title: {
+    fontSize: 33,
+    fontWeight: 'bold',
+    color: theme.main,
+    alignItems: 'flex-start',
+    marginTop: -5,
+  },
+  title2: {
+    marginTop: 70,
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: theme.main,
+  },
+  title3: {
+    marginTop: 40,
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: theme.main,
+  },
+  quote: {
+    fontSize: 18,
+    fontWeight: '600',
+    color: theme.main,
+    marginLeft: 10,
+  },
+  author: {
+    fontSize: 15,
+    fontWeight: '600',
+    color: '#595959',
+    marginRight: 20,
+    marginTop: 10,
+    textAlign: 'right',
+  },
+  todo: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: theme.main,
+    marginRight: 45
+  },
+  duedate: {
+    fontSize: 15,
+    fontWeight: '600',
+    color: '#595959',
+  },
+});
+
+  export const imageStyles = StyleSheet.create({
+    image: {
+      width: 116,
+      height: 154,
+      marginTop: 150,
+      marginBottom: 50,
+    },
+    image2: {
+      width: 40,
+      height: 43,
+      marginRight: 20,
+    },
+});
+
+export const buttonStyles = StyleSheet.create({
+  button: {
+    backgroundColor: '#314c1c',
+    alignItems: 'center',
+    justifyContent: 'center',
+    top: 20,
+    width: 202,
+    height: 40,
+    borderRadius: 5,
+  },
+  button2: {
+    backgroundColor: '#565656',
+    alignItems: 'center',
+    justifyContent: 'center',
+    top: 20,
+    left: 10,
+    width: 85,
+    height: 40,
+    borderRadius: 5,
+  },
+  title: {
+    fontSize: 13,
+    fontWeight: 'bold',
+    color: '#ffffff',
+  },
+});
