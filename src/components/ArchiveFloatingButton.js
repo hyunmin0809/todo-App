5737a2f53d16aafd9b2f7b20b53da6258fc6e95f
gkim <kimkm0419@naver.com>
2021-11-24 12:38:10 +0900

    Create Archive_FloatingButton

---
+++ src/components/ArchiveFloatingButton.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { View, Text, StyleSheet, Animated, TouchableWithoutFeedback } from "react-native";
+import { AntDesign, Entypo } from "@expo/vector-icons";
+
+export default class ArchiveFloatingButton extends React.Component {
+  render() {
+    return (
+      <View style={[styles.container, this.props.style]}>
+        <TouchableWithoutFeedback>
+          <Animated.View style={[styles.button, styles.menu]}>
+            <AntDesign name="inbox" size={24} color="#474E57" />
+          </Animated.View>
+        </TouchableWithoutFeedback>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    position: "absolute",
+  },
+  button: {
+    position: "absolute",
+    width: 60,
+    height: 60,
+    borderRadius: 60 / 2,
+    borderWidth: 3,
+    borderColor: "#474E57",
+    alignItems: "center",
+    justifyContent: "center",
+    shadowRadius: 10,
+    shadowColor: "#474E57",
+    shadowOpacity: 0.3,
+    shadowOffset: { height: 10 },
+  },
+  menu: {
+    backgroundColor: "#FFF"
+  }
+});
