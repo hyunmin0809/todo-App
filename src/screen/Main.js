9c99e5466bb4974be199bda3f304b3e39347bc6c
DAYOON0836 <89928584+DAYOON0836@users.noreply.github.com>
2021-12-13 17:24:30 +0900

    select/deselect
    1. select/deselect
    2. 전체 삭제, 선택 삭제 구현

--- src/screen/Main.js
+++ src/screen/Main.js
@@ -65,15 +65,72 @@ function Main({navigation}) {
     _saveTasks(currentTasks);
   }
 
+  // task 제거 modal과 연결 필요
+  const _deleteTask = id => {
+    const currentTasks = Object.assign({}, taskInfo);
+    delete currentTasks[id];
+    _saveTasks(currentTasks);
+  };
+/* Select/Deselect */
+  const [selectedItems, setSelectedItems] = useState([]);
+
+  const getSelected = id => selectedItems.includes(id.id);
+
+  const deSelectItems = () => setSelectedItems([]);
+
+  const selectItems = item => {
+    if (selectedItems.includes(item.id)) {
+      const newListItems = selectedItems.filter(
+        Task => Task !== item.id,
+      );
+      return setSelectedItems([...newListItems]);
+    }
+    setSelectedItems([...selectedItems, item.id]);
+  };
+
+// select task 제거
+const _SdeleteTask = () => {
+    const currentTasks = Object.assign({}, taskInfo);
+    for(var i = 0; i < selectedItems.length; i++){
+      var c = selectedItems[i];
+      delete currentTasks[c];
+      setSelectedItems([]);
+    }
+    _saveTasks(currentTasks);
+  };
+// 모든 task 제거
+  const _deleteTaskAll = id => {
+    const currentTasks = Object.assign({}, taskInfo);
+    if (id) {
+    for(const id in currentTasks){
+        delete currentTasks[id];
+     }
+     setSelectedItems([]);
+     _saveTasks(currentTasks);
+    }
+  };
+
+  // 모든 task 선택
+  const _selectAllItems = () => {
+    const currentTasks = Object.assign({}, taskInfo);
+    for(const id in currentTasks){
+      if (!selectedItems.includes(id))
+        selectedItems.push(id);
+      }
+    console.log(selectedItems)
+  };
 
   function DefaultTasks() { /*오늘 이후의 것만 나옴 */
     if(isEmpty === false){return (
-        <>
+        <Pressable onPress={deSelectItems}>
           <FlatList 
             data = {Object.values(sorted)}
-            renderItem = {({item}) =>  <Task key = {item.id} item = {item} toggleTask = {_toggleTask}/>}  
+            renderItem={({item}) => (
+            <Task key = {item.id} item = {item} deleteTask = {_deleteTask} toggleTask = {_toggleTask} onPress={() => handleOnPress(item)} onLongPress={() => selectItems(item)} selected={getSelected(item)} /> 
+            )}
+            keyExtractor={item => item.id}
           />
-        </>
+        </Pressable>
     )}
     else {return(null)}
   }
@@ -84,14 +141,18 @@ function Main({navigation}) {
         title="+"
         onPress={()=>navigation.navigate('Addtodo')}/>
       <Button
-        title="다 삭제하기"
-        onPress={()=> AsyncStorage.clear()}/>
+        title="삭제하기" //select task 제거
+        onPress={_SdeleteTask}/>
+      <Button
+        title="전체 삭제하기" //전체 task 제거
+        onPress={_deleteTaskAll}/>
+      <Button
+        title="전체 선택하기" //전체 task 선택(log로만 확인 가능)
+        onPress={_selectAllItems} />
       <DefaultTasks/>
     </View>
   );
     
 }
-
-
-  
+ 
   export default Main;
